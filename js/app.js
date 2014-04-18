ee7fb388f1663b33aa40d36bdd929d045e7f9ba5
Nick <sdwax@ukr.net>
2014-04-19 01:04:59 +0300

    add route

--- js/app.js
+++ js/app.js
@@ -1,4 +1,21 @@
 var goodsApp = angular.module('goodsApp', [
     'goodsControllers',
-    'goodsFilters'
+    'goodsFilters',
+    'ngRoute'
 ]);
+
+goodsApp.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider
+            .when('/Task-for-certification', {
+                templateUrl: 'partials/list.html',
+                controller: 'GoodsListCtrl'
+            })
+            .when('/Task-for-certification/:goodName', {
+                templateUrl: 'partials/detail.html',
+                controller: 'DetailCTRL'
+            })
+            .otherwise({
+                redirectTo: '/Task-for-certification'
+            });
+    }]);
